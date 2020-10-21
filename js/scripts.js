c1f7db5a1a8af995a56ca54d0d157160d88a654f
Eric Endsley <eric.endsley4@gmail.com>
2020-10-21 14:17:29 -0700

    made our calculator form functional and added output section to display result on the page
    Co-authored-by: Agata Kolodziej <agatakolohe@gmail.com>
    co-authored-by: Randel Moore <randel.c.moore@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -19,9 +19,12 @@ function multiply(number1, number2) {
 	return number1 * number2;
 }
 
-
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2)
-
-alert(result);
+$(document).ready(function() {
+	$("form#add").submit(function(event) {
+		event.preventDefault();
+		const number1 = parseInt($("input#add1").val());
+		const number2 = parseInt($("input#add2").val());
+		const result = add(number1, number2);
+		$("#output").text(result);
+	});
+});
