99a46b02a54bb6dcfece61ce55089fe1fc6bce2f
Eric Endsley <eric.endsley4@gmail.com>
2020-10-22 14:54:04 -0700

    added conditionals to perform the selected math operator
    Co-authored-by: Agata Kolodziej <agatakolohe@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -20,18 +20,21 @@ function multiply(number1, number2) {
 }
 
 $(document).ready(function() {
-	$("form#add").submit(function(event) {
+	$("form#calculator").submit(function(event) {
 		event.preventDefault();
-		const number1 = parseInt($("input#add1").val());
-		const number2 = parseInt($("input#add2").val());
-		const result = add(number1, number2);
-		$("#output").text(result);
-	});
-	$("form#subtract").submit(function(event) {
-		event.preventDefault();
-		const number1 = parseInt($("input#subtract1").val());
-		const number2 = parseInt($("input#subtract2").val());
-		const result = subtract(number1, number2);
+		const number1 = parseInt($("input#num1").val());
+		const number2 = parseInt($("input#num2").val());
+		const operator = $("input:radio[name=operator]:checked").val();
+		let result;
+		if (operator === "add") {
+			result = add(number1, number2);
+		} else if (operator === "subtract") {
+			result = subtract(number1, number2);
+		} else if (operator === "multiply") {
+			result = multiply(number1, number2);
+		} else if (operator === "divide") {
+			result = divide(number1, number2);
+		}
 		$("#output").text(result);
 	});
 });
